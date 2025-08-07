3c922feb12a1d1dc54895d7c99732f4a63ed3f04
kuwabara <kuwabara@jig.jp>
2025-08-07 14:40:07 +0900

    👍 期間指定コンポーネントを共通化した

--- packages/shared/src/components/index.ts
+++ packages/shared/src/components/index.ts
@@ -1,4 +1,5 @@
 // 共有コンポーネントをここにエクスポート
 // 例: export { Button } from './Button';
 export { MonthRangePicker } from "./parts/month-range-picker";
+export { RangeSelector } from "./parts/range-selector";
 export { TypeSelect } from "./parts/type-select";
