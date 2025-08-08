61456c02f445e9c075b11e68a0403604e43c134c
yamaji <yamaji@jig.jp>
2025-08-08 10:33:33 +0900

    👍 csvを環境に応じた場所からfetchする処理を実装

---
+++ packages/shared/src/utils/getData.ts
@@ -0,0 +1,59 @@
+/// <reference types="vite/client" />
+import Papa from "papaparse";
+import { AggregatedData, AggregatedRange, DateNumbers, ObjectClass, Placement } from "../types";
+
+declare const __CSV_ORIGIN__: string;
+
+type getRawDataProps<T extends AggregatedRange> = T extends "full"
+  ? {
+      placement: Placement;
+      objectClass: ObjectClass;
+      aggregateRange: T;
+      kind?: "people-flow-data";
+    }
+  : {
+      placement: Placement;
+      objectClass: ObjectClass;
+      aggregateRange: T;
+      kind?: "people-flow-data";
+      date: Date | DateNumbers<T>;
+    };
+
+export async function getRawData<T extends AggregatedRange>(params: getRawDataProps<T>) {
+  const { placement, objectClass, aggregateRange, kind = "people-flow-data" } = params;
+  let url = `${__CSV_ORIGIN__}/data/${kind}/${aggregateRange}/${placement}/${objectClass}`;
+
+  if (aggregateRange === "full") {
+    url += ".csv";
+  } else if (aggregateRange === "monthly") {
+    const { year, month } =
+      params.date instanceof Date
+        ? { year: params.date.getFullYear(), month: params.date.getMonth() }
+        : params.date;
+    url += `/${year}/${year}-${month}.csv`;
+  } else if (aggregateRange === "daily") {
+    const { year, month, day } =
+      params.date instanceof Date
+        ? {
+            year: params.date.getFullYear(),
+            month: params.date.getMonth(),
+            day: params.date.getDate(),
+          }
+        : params.date;
+    url += `/${year}/${month}/${year}-${month}-${day}.csv`;
+  } else {
+    const { year, month, day, hour } =
+      params.date instanceof Date
+        ? {
+            year: params.date.getFullYear(),
+            month: params.date.getMonth(),
+            day: params.date.getDate(),
+            hour: params.date.getHours(),
+          }
+        : params.date;
+    url += `/${year}/${month}/${day}/${year}-${month}-${day}-${hour}.csv`;
+  }
+
+  const rawData = Papa.parse<AggregatedData>(url, { header: true }).data;
+  return rawData;
+}
--- packages/shared/src/utils/index.ts
+++ packages/shared/src/utils/index.ts
@@ -1,2 +1,5 @@
 // 共有ユーティリティ関数をここにエクスポート
 // 例: export { formatDate } from './dateUtils';
+
+export * from "./date";
+export * from "./getData";
