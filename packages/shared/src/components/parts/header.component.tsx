6666dc1aaffbe3efb7fce99ed2f522cef04429b9
kuwabara <kuwabara@jig.jp>
2025-08-08 16:47:52 +0900

    👍 ヘッダーのスタイルを調整

--- packages/shared/src/components/parts/header.component.tsx
+++ packages/shared/src/components/parts/header.component.tsx
@@ -4,7 +4,7 @@ export function Header() {
   return (
     <header className="relative border-b-gray-500 flex h-12 w-full items-center justify-between gap-x-2 border-b-2 pb-2">
       <div className="flex flex-col lg:flex-row w-full items-center justify-center lg:justify-start gap-x-2">
-        <h1 className="lg:absolute lg:left-1/2 lg:top-1/2 lg:-translate-x-1/2 lg:-translate-y-1/2 lg:text-4xl text-2xl font-bold whitespace-nowrap pb-3">
+        <h1 className="sm:absolute sm:left-1/2 sm:top-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2 sm:text-4xl text-2xl font-bold whitespace-nowrap pb-3">
           福井駅東口案内所 人流データ
         </h1>
       </div>
