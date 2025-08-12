207ea2b4018f50e593c00702952ff3ed7fb472f9
yamaji <yamaji@jig.jp>
2025-08-12 17:41:49 +0900

    👍 ヘッダーのプレースホルダーを作成

--- packages/rainbow-line/src/App.tsx
+++ packages/rainbow-line/src/App.tsx
@@ -1,5 +1,12 @@
+import { HeaderPlaceHolder } from "./components/ph-header";
+
 function App() {
-  return <span>rainbow-line</span>;
+  return (
+    <div className="flex flex-col w-full h-full min-h-[100dvh] p-4">
+      <HeaderPlaceHolder title="レインボーライン" />
+      <span>rainbow-line</span>
+    </div>
+  );
 }
 
 export default App;
