c4881a26a5d3dad9d6c646cea20250750ed44b4f
yamaji <yamaji@jig.jp>
2025-08-13 15:04:43 +0900

    👍 フィルターのサンプル表示をレインボーライングラフページに追加

--- packages/rainbow-line/src/App.tsx
+++ packages/rainbow-line/src/App.tsx
@@ -1,10 +1,11 @@
+import { FiltersSample } from "./components/parts/filters";
 import { HeaderPlaceHolder } from "./components/parts/ph-header";
 
 function App() {
   return (
     <div className="flex flex-col w-full h-full min-h-[100dvh] p-4">
       <HeaderPlaceHolder title="レインボーライン" />
-      <span>rainbow-line</span>
+      <FiltersSample className="pt-4" />
     </div>
   );
 }
---
+++ packages/rainbow-line/src/components/parts/filters.tsx
@@ -0,0 +1,61 @@
+import { OBJECT_CLASS_ATTRIBUTES, PLACEMENTS, REGIONS_PREFECTURES } from "@fukui-kanko/shared";
+import { SelectScrollable } from "@fukui-kanko/shared/components/parts";
+import { cn } from "@fukui-kanko/shared/utils";
+
+const filterAttributes: Array<{ items: { text: string; value: string }[]; name: string }> = [
+  {
+    name: "駐車場",
+    items: [
+      { text: "-", value: "reset" },
+      ...Object.entries(PLACEMENTS)
+        .filter(([key]) => key.includes("rainbow-line"))
+        .map(([key, value]) => {
+          return { text: value.text, value: key };
+        }),
+    ],
+  },
+  {
+    name: "地方",
+    items: [
+      { text: "-", value: "reset" },
+      ...Object.entries(REGIONS_PREFECTURES).map(([key, value]) => {
+        return { text: value.name, value: key };
+      }),
+    ],
+  },
+  {
+    name: "都道府県",
+    items: [
+      { text: "-", value: "reset" },
+      ...Object.entries(OBJECT_CLASS_ATTRIBUTES.LicensePlate.prefectures).map(([key, value]) => {
+        return { text: value, value: key };
+      }),
+    ],
+  },
+  {
+    name: "車両分類",
+    items: [
+      { text: "-", value: "reset" },
+      ...Object.entries(OBJECT_CLASS_ATTRIBUTES.LicensePlate.carCategories).map(([key, value]) => {
+        return { text: value, value: key };
+      }),
+    ],
+  },
+];
+
+export function FiltersSample({ className }: { className?: string }) {
+  return (
+    <ul className={cn(className, "flex w-full justify-center items-start gap-4")}>
+      {filterAttributes.map((attribute, index) => (
+        <li className="flex flex-col items-center" key={`${index}-${attribute.name}`}>
+          <span className="font-bold text-center">{attribute.name}</span>
+          <SelectScrollable
+            items={attribute.items}
+            onChange={alert}
+            className="w-fit h-40 overflow-auto border-2 border-border rounded-lg flex flex-col gap-y-2"
+          ></SelectScrollable>
+        </li>
+      ))}
+    </ul>
+  );
+}
