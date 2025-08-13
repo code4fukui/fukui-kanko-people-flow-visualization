880e8be1e4630dfe29ff7af40c7491addb48ad41
yamaji <yamaji@jig.jp>
2025-08-13 17:11:19 +0900

    👍 フィルターにデフォルト値を設定するように

--- packages/rainbow-line/src/components/parts/filters.tsx
+++ packages/rainbow-line/src/components/parts/filters.tsx
@@ -2,48 +2,67 @@ import { OBJECT_CLASS_ATTRIBUTES, PLACEMENTS, REGIONS_PREFECTURES } from "@fukui
 import { SelectScrollable } from "@fukui-kanko/shared/components/parts";
 import { cn } from "@fukui-kanko/shared/utils";
 
-const filterAttributes: Array<{ items: { text: string; value: string }[]; name: string }> = [
+const selectAllOption = { text: "すべて", value: "all" } as const;
+
+const filterAttributes: Array<{
+  items: { text: string; value: string }[];
+  name: string;
+  id: string;
+}> = [
   {
     name: "駐車場",
+    id: "parkingLot",
     items: [
-      { text: "-", value: "reset" },
+      selectAllOption,
       ...Object.entries(PLACEMENTS)
         .filter(([key]) => key.includes("rainbow-line"))
         .map(([key, value]) => {
           return { text: value.text, value: key };
         }),
-    ],
-  },
+    ] as const,
+  } as const,
   {
     name: "地方",
+    id: "region",
     items: [
-      { text: "-", value: "reset" },
+      selectAllOption,
       ...Object.entries(REGIONS_PREFECTURES).map(([key, value]) => {
         return { text: value.name, value: key };
       }),
-    ],
-  },
+    ] as const,
+  } as const,
   {
     name: "都道府県",
+    id: "prefecture",
     items: [
-      { text: "-", value: "reset" },
+      selectAllOption,
       ...Object.entries(OBJECT_CLASS_ATTRIBUTES.LicensePlate.prefectures).map(([key, value]) => {
         return { text: value, value: key };
       }),
-    ],
-  },
+    ] as const,
+  } as const,
   {
     name: "車両分類",
+    id: "carCategory",
     items: [
-      { text: "-", value: "reset" },
+      selectAllOption,
       ...Object.entries(OBJECT_CLASS_ATTRIBUTES.LicensePlate.carCategories).map(([key, value]) => {
         return { text: value, value: key };
       }),
-    ],
-  },
-];
+    ] as const,
+  } as const,
+] as const;
 
-export function FiltersSample({ className }: { className?: string }) {
+export function FiltersSample({
+  className,
+  onFilterChange = () => {},
+}: {
+  className?: string;
+  onFilterChange?: (
+    attribute: (typeof filterAttributes)[number]["id"],
+    value: (typeof filterAttributes)[number]["items"][number]["value"],
+  ) => void;
+}) {
   return (
     <ul className={cn(className, "flex w-full justify-center items-start gap-4")}>
       {filterAttributes.map((attribute) => (
@@ -51,7 +70,8 @@ export function FiltersSample({ className }: { className?: string }) {
           <span className="font-bold text-center">{attribute.name}</span>
           <SelectScrollable
             items={attribute.items}
-            onChange={alert}
+            onChange={(v) => onFilterChange(attribute.id, v)}
+            defaultValue="all"
             className="w-fit h-40 border-2 border-border rounded-lg flex flex-col gap-y-2"
           ></SelectScrollable>
         </li>
