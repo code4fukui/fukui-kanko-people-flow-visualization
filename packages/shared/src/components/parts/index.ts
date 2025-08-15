b8e6bd5cb438c0d0cef1450fd770ed83ea228a56
kuwabara <kuwabara@jig.jp>
2025-08-15 10:55:31 +0900

    👍 period-graph-panelコンポーネントのファイル名を変更した

--- packages/shared/src/components/parts/index.ts
+++ packages/shared/src/components/parts/index.ts
@@ -3,7 +3,7 @@ export * from "./graph.component";
 export * from "./loading-spinner.component";
 export * from "./month-picker.component";
 export * from "./month-range-picker.component";
-export * from "./period-graph-panel";
+export * from "./period-graph-panel.component";
 export * from "./range-selector.component";
 export * from "./select-scrollable";
 export * from "./stats-summary.component";
