80f57f56ddc936d88ea95451cf8a937e2938c792
yamaji <yamaji@jig.jp>
2025-08-15 18:19:00 +0900

    wip

--- packages/rainbow-line/src/components/parts/rainbow-line-chart-panel.tsx
+++ packages/rainbow-line/src/components/parts/rainbow-line-chart-panel.tsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
-import { Graph, RangeSelector } from "@fukui-kanko/shared/components/parts";
+import { useEffect, useState } from "react";
+import { RangeSelector } from "@fukui-kanko/shared/components/parts";
+import { ChartConfig, ChartContainer } from "@fukui-kanko/shared/components/ui";
 import { AggregatedData } from "@fukui-kanko/shared/types";
 import { cn, getMaxDate } from "@fukui-kanko/shared/utils";
+import { CartesianGrid, ComposedChart, XAxis, YAxis } from "recharts";
 
 export function RainbowLineChartPanel({
   data,
@@ -10,6 +12,12 @@ export function RainbowLineChartPanel({
   data: AggregatedData[];
   className?: string;
 }) {
+  const [chartConfig, setChartConfig] = useState<ChartConfig>({
+    "total count": {
+      label: "ナンバープレート検出回数",
+    },
+  });
+
   const [graphRange, setGraphRange] = useState<{
     from: Date | undefined;
     to: Date | undefined;
@@ -17,13 +25,21 @@ export function RainbowLineChartPanel({
     from: new Date("2024-12-20"),
     to: getMaxDate(),
   });
-  const dataInRange = data.filter((row) => {
-    const aggregatedFrom = new Date(row["aggregate from"]);
-    return (
-      aggregatedFrom >= (graphRange.from || new Date(0)) &&
-      aggregatedFrom < (graphRange.to || new Date())
-    );
-  });
+  const [dataInRange, setDataInRange] = useState<AggregatedData[]>([]);
+
+  useEffect(
+    () =>
+      setDataInRange(
+        data.filter((row) => {
+          const aggregatedFrom = new Date(row["aggregate from"]);
+          return (
+            aggregatedFrom >= (graphRange.from || new Date(0)) &&
+            aggregatedFrom < (graphRange.to || new Date())
+          );
+        }),
+      ),
+    [data, graphRange],
+  );
 
   return (
     <div
@@ -41,20 +57,20 @@ export function RainbowLineChartPanel({
       ></RangeSelector>
 
       <div className="flex flex-col gap-y-4 w-full min-w-full grow overflow-auto">
-        <Graph
-          data={dataInRange}
-          type="day"
-          xKey="aggregate from"
-          yKey="total count"
-          className="w-full min-h-[calc(100dvh_-_(32px_+_48px_+_200px_+_16px_+_62px_+_16px))]"
-        />
-        <Graph
-          data={dataInRange}
-          type="day"
-          xKey="aggregate from"
-          yKey="total count"
-          className="w-full min-h-[calc(100dvh_-_400px)]"
-        />
+        <ChartContainer config={chartConfig}>
+          <ComposedChart>
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="aggregate from"
+              tickFormatter={(value) => new Date(value).toLocaleDateString()}
+              tick={{ fontSize: 12 }}
+              height={50}
+              interval={0}
+              tickCount={dataInRange.length > 10 ? 10 : dataInRange.length}
+            />
+            <YAxis dataKey={"total count"} tickLine={true} allowDecimals={false} />
+          </ComposedChart>
+        </ChartContainer>
       </div>
     </div>
   );
