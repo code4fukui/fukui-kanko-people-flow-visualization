a38eb1cfbb463e3bf46307dd84a155445a85e82b
yamaji <yamaji@jig.jp>
2025-08-22 10:14:09 +0900

    👍 ヘッダーのフォントサイズを修正

--- packages/rainbow-line/src/components/parts/header.tsx
+++ packages/rainbow-line/src/components/parts/header.tsx
@@ -2,9 +2,9 @@ import { ExternalNavigation } from "@fukui-kanko/shared/components/parts";
 
 export function Header({ title }: { title: string }) {
   return (
-    <header className="h-12 grid grid-cols-3 w-full items-center justify-center border-b-2 border-border pb-2">
-      <span></span>
-      <h1 className="w-full text-center text-2xl font-bold">{title}</h1>
+    <header className="h-12 grid grid-cols-[1fr_auto_1fr] w-full items-center justify-center border-b-2 border-border pb-2">
+      <div />
+      <h1 className="text-center text-4xl font-bold">{title}</h1>
       <div className="w-fit place-self-end">
         <ExternalNavigation />
       </div>
